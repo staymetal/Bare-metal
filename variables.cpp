4fd88dab51bb033c1577296515b587cc895c49cb
staymetal <surya.poojary@yandex.com>
2017-10-09 18:03:53 +0530

    variables.cpp in

---
+++ variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main(){
+
+  int num = 4;
+  bool awesome = true;
+  char letter = 'A';
+
+  int list[]={1,2,3,4,5,6,7,8,9,0};
+
+  char name[]="surya";
+
+  std::cout << "The size of num is " << sizeof(num) << std::endl;
+
+  std::cout << "The size of awesome is " << sizeof(awesome) << std::endl;
+
+  std::cout << "The size of letter is " << sizeof(letter) << std::endl;
+
+  std::cout << "The size of list is " << sizeof(list) << std::endl;
+
+  std::cout << "The size of name is " << sizeof(name) << std::endl;
+  
+
+  return 0;
+
+}
+
+  
